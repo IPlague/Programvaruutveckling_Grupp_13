9a7c7a67a6390f043cbb6476b479e83b938c2389
Alessandro Grosso <ale04.grosso@gmail.com>
2025-12-02 20:13:10 +0100

    Fixed c image errors

--- ESP32/ESP32-T4-S3-Skeleton-main/project/icons/Cloudy.c
+++ ESP32/ESP32-T4-S3-Skeleton-main/project/icons/Cloudy.c
@@ -78,10 +78,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMAGE_CLOUDY 
 
 
 const lv_img_dsc_t Cloudy = {
-  .header.always_zero = 0,
-  .header.w = 50,
-  .header.h = 50,
-  .data_size = 2500 * 2,
-  .header.cf = LV_IMG_CF_TRUE_COLOR,
-  .data = Cloudy_map,
+    {0, 50, 50, LV_IMG_CF_TRUE_COLOR}, // header: always_zero, w, h, cf
+    50 * 50 * 2,                        // data_size
+    Cloudy_map                           // data
 };
